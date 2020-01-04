b71463d1b6d6ea95b09951ee78600e039486f834
CN-GuanHao <994561575@qq.com>
2020-01-04 22:17:21 +0800

    订单管理添加分页

--- src/pages/order/List.vue
+++ src/pages/order/List.vue
@@ -3,13 +3,15 @@
         <!-- 按钮 -->
         <!-- /按钮结束 -->
         <!-- 表格 -->
-       <el-table :data="orders">
+       <el-table :data="orders.list">
            <el-table-column prop="id" label="订单编号" ></el-table-column>
            <el-table-column prop="orderTime" label="下单时间" ></el-table-column>
            <el-table-column prop="total" label="总价" ></el-table-column>
            <el-table-column prop="status" label="状态" ></el-table-column>
             <el-table-column prop="customerId" label="顾客id" ></el-table-column>
-           <el-table-column label="操作" >
+           <el-table-column prop="waiterId" label="员工ID"></el-table-column>
+           <el-table-column prop="addressId" label="地址ID" ></el-table-column>
+           <el-table-column fixed="right" label="操作" >
                <template v-slot="slot">
                     <a href="">详情</a>
                </template>
@@ -17,8 +19,7 @@
        </el-table>
         <!-- /表格结束 --> 
         <!-- 分页开始 -->
-        <!-- <el-pagination layout="prev, pager, next" :total="50"> -->
-        <!-- </el-pagination> -->
+        <el-pagination layout="prev, pager, next" :total="orders.total" @current-change="pageChangeHandler"></el-pagination>
         <!-- /分页结束 -->
         <!-- 模态框 -->
          <!-- /模态框结束 -->       
@@ -31,32 +32,42 @@ import querystring from 'querystring'
 export default {
     //暴露接口,用于存放网页中需要调用的方法
     methods:{
-        loadData(){
-            let url = "http://localhost:6677/order/findAll"
-            request.get(url).then((response)=>{
-            //将查询结果设置到customers中，this指向外部函数的this
-            this.orders = response.data;
-        })
+        pageChangeHandler(page){
+            this.params.page = page-1;
+            this.loadData();
         },
-        
-        closeModalHandler(){
-            this.visible=false;
+        loadData(){
+            let url = "http://localhost:6677/order/queryPage"
+            request({
+                url,
+                method:"post",
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                },
+                data:querystring.stringify(this.params)
+            }).then((response)=>{
+                this.orders = response.data;
+            })
         }
     },
     //用于存放要想网页中显示的数据
     data(){
         return{
             visible:false,
-            orders:[],
+            orders:{},
             form:{
                 type:"order"
+            },
+            params:{
+                page:0,
+                pageSize:10
             }
         }
     },
     created(){
         //this为当前vue实例对象
         //vue实例完毕
-        this.loadData()
+        this.loadData();
     }    
 }
 </script>
